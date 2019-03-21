ba7cb5902c3dacb501e4b61c1b9535ea7578cf6a
hiboo <hiboo.lee@qq.com>
2019-03-21 17:06:02 +0800

    hello

--- Hello.java
+++ Hello.java
@@ -1,7 +1,19 @@
 public class Hello
 {
+	String name;
+	int age;
+	public void setNmae(String name)
+	{
+		this.name=name;
+	}
+	public String getName() {
+		return name;
+	}
 	public static void main(String[] args)
 	{
-		System.out.println("hello");
+		Hello h=new Hello();
+		h.setNmae("hello");
+		
+		System.out.println(h.getName());
 	}
 }
